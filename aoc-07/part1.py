10511f43fee42f116de41a0910aa16d0337511ed
jmcortesa <jmcortesa@aragon.es>
2022-12-07 10:23:16 +0100

    aoc-07

---
+++ aoc-07/part1.py
@@ -0,0 +1,45 @@
+import re
+from pathlib import Path
+
+# file_input = "data-training.txt"
+file_input = "data.txt"
+
+cwd = Path("/")
+dirtree: dict[Path, int] = {cwd: 0}
+
+
+def process(cmd: str):
+    global cwd
+    if cmd.startswith("$"):
+        if cmd == "$ ls":
+            pass
+        elif cmd == "$ cd ..":
+            cwd = cwd.parent
+        else:  # $ cd dir
+            cwd = cwd / cmd[5:]
+    elif cmd.startswith("dir"):
+        p = cmd[4:]
+        dirtree[cwd / p] = 0
+    else:
+        size = int(cmd.split()[0])
+        dirtree[cwd] += size
+
+
+with open(file_input) as f:
+    cmds = [cmd[:-1] for cmd in f]
+
+for cmd in cmds:
+    process(cmd)
+
+
+# trail rest of dirs
+for parts in sorted((p.parts for p in dirtree.keys()), reverse=True):
+    if len(parts) == 1:
+        continue
+    p = Path(*parts)
+    dirtree[p.parent] += dirtree[p]
+
+
+print(sum(v for v in dirtree.values() if v <= 100000))
+
+print(dirtree)
