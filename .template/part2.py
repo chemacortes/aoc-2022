0e5daa0e46362479675f5fc816d9c03bb337d8a7
chemacortes <devxtrem@gmail.com>
2022-12-21 20:02:14 +0100

    Template modified for easier run and debug

--- .template/part2.py
+++ .template/part2.py
@@ -1,6 +1,11 @@
+import os
+from pathlib import Path
+
 from util import read
 
 if __name__ == "__main__":
+
+    os.chdir(Path(__file__).parent)
 
     TRAINING = True
     data = read("data-training.txt" if TRAINING else "data.txt")
